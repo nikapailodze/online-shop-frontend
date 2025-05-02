0ac6c234aae4cbcd4f451ac9fea0abe3de27e485
nikapailodze <nikapail.a01@gmail.com>
2025-05-02 16:34:39 +0400

    feat: created information cardss on prod page

---
+++ app/proditempage/[id]/Components/InformationCard/InformationCard.tsx
@@ -0,0 +1,55 @@
+import React, { useRef, useState, useEffect } from "react";
+import styles from "./InformationCard.module.scss";
+import { GoPlus } from "react-icons/go";
+
+interface InformationCardProps {
+  title: string;
+  description: string;
+}
+
+const InformationCard: React.FC<InformationCardProps> = ({
+  title,
+  description,
+}) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const descRef = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState("0px");
+
+  useEffect(() => {
+    if (descRef.current) {
+      setHeight(isVisible ? `${descRef.current.scrollHeight}px` : "0px");
+    }
+  }, [isVisible]);
+
+  return (
+    <div className={styles.informationCard}>
+      <div className={styles.informationCardTitleWrapper}>
+        <h1 className={styles.heading3}>{title}</h1>
+        <button
+          onClick={() => setIsVisible(!isVisible)}
+          className={styles.informationCardButton}
+        >
+          <GoPlus
+            size={24}
+            color="white"
+            className={`${styles.informationCardIcon} ${
+              isVisible ? styles.active : ""
+            }`}
+          />
+        </button>
+      </div>
+
+      <div
+        ref={descRef}
+        className={`${styles.descriptionWrapper} ${
+          isVisible ? styles.descriptionWrapperVisible : ""
+        }`}
+        style={{ height }}
+      >
+        <p className={styles.bodyheading}>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default InformationCard;
