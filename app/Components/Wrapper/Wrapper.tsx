219c906271ebc50c337c2d3ce830d5ebda47e0bd
nikapailodze <nikapail.a01@gmail.com>
2025-05-05 15:01:16 +0400

    fix: dont show nav on login/signup

--- app/Components/Wrapper/Wrapper.tsx
+++ app/Components/Wrapper/Wrapper.tsx
@@ -1,17 +1,22 @@
+'use client'
 import styles from "./Wrapper.module.scss";
 import React, { ReactNode } from "react";
 import Navigaton from "../Navigaton/Navigaton";
 import { CartRefProvider } from "@/app/Context/CartRefContext";
+import { usePathname } from "next/navigation";
 
 interface WrapperProps {
   children: ReactNode;
 }
+const excludedPaths = ["/login", "/signup"];
 
 const Wrapper: React.FC<WrapperProps> = ({ children }) => {
+  const pathName = usePathname();
+  const isExcludedPath = excludedPaths.includes(pathName);
   return (
     <div className={styles.container}>
       <CartRefProvider>
-        <Navigaton />
+        {!isExcludedPath && <Navigaton />}
         <div className={styles.wrapper}>{children}</div>
       </CartRefProvider>
     </div>
