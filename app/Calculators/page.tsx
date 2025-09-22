3722d0b989509ee860c67bff52b379cdb5b7ecce
nikapailodze <nikapail.a01@gmail.com>
2025-09-22 18:08:20 -0400

    refeat: styled title and subtitle

--- app/Calculators/page.tsx
+++ app/Calculators/page.tsx
@@ -4,12 +4,23 @@ import { diabetesRiskMeta } from "./diabetes-risk/meta";
 import { drsGriffinMeta } from "./diabetes-risk-griffin-2000/meta";
 import CardComponent from "../Components/CalculatorSection/CardComponent/CardComponent";
 
-const calculators = [diabetesRiskMeta, drsGriffinMeta,drsGriffinMeta,diabetesRiskMeta];
+const calculators = [
+  diabetesRiskMeta,
+  drsGriffinMeta,
+  drsGriffinMeta,
+  diabetesRiskMeta,
+];
 
 export default function CalculatorsIndex() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>Endocrinology Calculators</h1>
+      <div className={styles.titlesWrapper}>
+        <h1 className={styles.heading}>Endocrinology Calculators</h1>
+        <p className={styles.subTitle}>
+          Tools for assessing hormone levels, medication dosages, and other
+          endocrine parameters.
+        </p>
+      </div>
       <div className={styles.cardsWrapper}>
         {calculators.map((c) => (
           <CardComponent
