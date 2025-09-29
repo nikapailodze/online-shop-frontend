6e391ae8bb3a4e79a1ae35c5b793d3a642b10c75
nikapailodze <nikapail.a01@gmail.com>
2025-09-29 11:46:37 -0400

    fix: scrolling bug on responsive

--- app/Components/HomeSection/HomeSection.tsx
+++ app/Components/HomeSection/HomeSection.tsx
@@ -1,28 +1,10 @@
 "use client";
-import { useEffect, useState } from "react";
 import styles from "./HomeSection.module.scss";
 
 const HomeSection = () => {
-  const [scale, setScale] = useState(1);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.scrollY;
-      const maxScroll = window.innerHeight;
-      const newScale = Math.max(0.8, 1 - (scrollTop / maxScroll) * 0.2);
-      setScale(newScale);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <section className={styles.homeSectionWrapper}>
-      <div
-        className={styles.homeSectionContent}
-        style={{ transform: `scale(${scale})` }}
-      >
+      <div className={styles.homeSectionContent}>
         <div className={styles.heroText}>
           <h1 className={styles.title}>Welcome to EndoPie</h1>
           <p className={styles.title}>Smart Calculators for Smarter Care</p>
